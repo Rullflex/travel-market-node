1d121079d0ad37aa5d8d62efd81b41be27cd1bae
Dmitry Gorshkov <libafer99@gmail.com>
2025-08-25 21:27:28 +0500

    chore: eslint console off

--- eslint.config.js
+++ eslint.config.js
@@ -4,5 +4,6 @@ export default antfu({}, {
   rules: {
     'n/prefer-global/process': ['off'],
     'style/brace-style': ['error', '1tbs', { allowSingleLine: true }],
+    'no-console': 'off',
   },
 })
