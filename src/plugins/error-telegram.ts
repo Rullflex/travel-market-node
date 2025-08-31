ce50579cd5857392c4dc599e73001467f8891f38
Dmitry Gorshkov <libafer99@gmail.com>
2025-08-31 15:13:46 +0500

    fix: проверка на наличие констант для тг логирования

--- src/plugins/error-telegram.ts
+++ src/plugins/error-telegram.ts
@@ -3,6 +3,10 @@ import fp from 'fastify-plugin'
 import { NOTIFICATIONS_BOT_TOKEN, NOTIFICATIONS_CHAT_ID } from '@/const/index.js'
 
 export default fp(async (fastify) => {
+  if (!NOTIFICATIONS_BOT_TOKEN || !NOTIFICATIONS_CHAT_ID) {
+    return
+  }
+
   fastify.addHook('onError', async (request, reply, error) => {
     try {
       axios.post(`https://api.telegram.org/bot${NOTIFICATIONS_BOT_TOKEN}/sendMessage`, {
