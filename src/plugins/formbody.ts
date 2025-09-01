fe3c7ff0a29cad46bc9a4047ea22e2dfdf0ad867
Dmitry Gorshkov <libafer99@gmail.com>
2025-09-01 13:32:28 +0500

    feat: add parser

--- src/plugins/formbody.ts
+++ src/plugins/formbody.ts
@@ -1,6 +1,7 @@
 import type { FastifyFormbodyOptions } from '@fastify/formbody'
 import formbody from '@fastify/formbody'
 import fp from 'fastify-plugin'
+import qs from 'qs'
 
 /**
  * This plugins adds a content type parser for the content type application/x-www-form-urlencoded
@@ -8,5 +9,5 @@ import fp from 'fastify-plugin'
  * @see https://github.com/fastify/fastify-formbody
  */
 export default fp<FastifyFormbodyOptions>(async (fastify) => {
-  fastify.register(formbody)
+  fastify.register(formbody, { parser: str => qs.parse(str) })
 })
